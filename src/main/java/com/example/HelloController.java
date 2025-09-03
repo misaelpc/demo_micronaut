76df63b9021943fc5666bd9426e7c80526d1944d
Misael <misaelpc@Misaels-MacBook-Pro-2.local>
2025-09-03 15:07:02 -0600

    Add unused import

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -3,7 +3,7 @@ package com.example;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Post;
-import io.micronaut.http.annotation.Body;
+import jakarta.inject.Inject;
 
 @Controller("/hello")
 public class HelloController {
