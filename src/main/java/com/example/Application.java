1c1eb0968c370eec0de16ed8a9b9cc3c0a16a8a0
Misael <misaelpc@Misaels-MacBook-Pro-2.local>
2025-09-03 23:28:49 -0600

    fix(java): resolve java:S1118 in Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,7 +2,9 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application { private Application() {}
+public class Application { 
+
+ private Application() {}
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
