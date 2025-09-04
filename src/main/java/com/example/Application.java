d2128b614e681e4776315579c3c59cce13e47b75
Misael <misaelpc@Misaels-MacBook-Pro-2.local>
2025-09-04 09:22:31 -0600

    fix(Application.java): add private constructor to hide the implicit public one

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -2,7 +2,7 @@ package com.example;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class Application { private Application() {}
 
     public static void main(String[] args) {
         Micronaut.run(Application.class, args);
